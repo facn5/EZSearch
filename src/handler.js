5b4604a6270c108d35336e4b812cd6e8e1f13212
obaydah <obayda7h@gmail.com>
2019-03-26 15:18:30 +0200

    error page handling

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,54 @@
 const path = require('path');
 const fs = require('fs');
 
-const handleHome = (res) => {
-    let filePath = path.join(__dirname,'..','public','index.html');
-        fs.readFile(filePath,(err, file)=>{
-            if(err){
-                res.writeHead(500);
-                res.end("500 error");
-            }else {
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(file);
-            }
-        }); 
+const exType = {
+    html: { 'Content-Type': 'text/html' },
+    css: { 'Content-Type': 'text/css' },
+    js: { 'Content-Type': 'application/javascript' }
+};
+
+const homeHandler = (res) => {
+    let filePath = path.join(__dirname, '..', 'public', 'index.html');
+    fs.readFile(filePath, 'utf8', (err, file) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("500 error");
+        } else {
+            res.writeHead(200, exType.html);
+            res.end(file);
+        }
+    });
+}
+
+const publicHandler = (res, url) => {
+    let filePath = path.join(__dirname, '..', url);
+    let extension = url.split('.')[1];
+    fs.readFile(filePath, (err, file) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('500 Error');
+        } else {
+            res.writeHead(200, exType[extension]);
+            res.end(file);
+        }
+    })
+}
+
+const errHandler = (res) => {
+    let filePath = path.join(__dirname, '..', 'public', '404.html');
+         fs.readFile(filePath, (err, file) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("500 error");
+        } else {
+            res.writeHead(200, exType.html);
+            res.end(file);
+        }
+    });
 }
 
 module.exports = {
-    handleHome: handleHome
+    homeHandler: homeHandler,
+    publicHandler: publicHandler,
+    errHandler: errHandler
 }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,4 @@ const router = require('./router');
 
 const server = http.createServer(router);
 
-server.listen(3000, () => {
-    console.log("SERVER IS UP");
-});
+server.listen(3000);
