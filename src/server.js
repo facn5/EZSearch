fcf8bbba4b1937e4045ee03e1b18fc639a566c6c
mrfong <therealshadix@gmail.com>
2019-03-27 16:22:04 +0200

    accesiblty fix for image alt

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var http = require("http");
 var router = require("./router");
 
 var server = http.createServer(router);
-var port = process.env.PORT || 3052;
+var port = process.env.PORT || 3152;
 
 server.listen(port, function() {
   console.log("Listening to port: " + port);
